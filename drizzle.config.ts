104dbdc99bbaad0f156fa5ea4e726131ed15ef54
soumen manik <soumen192004manik@gmail.com>
2026-03-19 15:43:13 +0530

    1stwwf

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_s7qo4efDZMpG@ep-noisy-surf-amdgjvq3-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+    url: process.env.DATABASE_URL as string,
   },
 });
